63412ca437755dcadbf429d03011df38f958776b
comsavvy <olusolaogundepo@gmail.com>
2020-12-02 09:04:41 +0100

    Different files for scraping engine
    I joined different paragraph using join method and I also create a file
    to do the scraping

---
+++ scraping_engine.py
@@ -0,0 +1,43 @@
+import scrapy
+from scrapy import Request, Selector, Spider
+from scrapy.crawler import CrawlerProcess
+from collections import defaultdict
+
+summary = defaultdict(list)
+
+class PunchScraper(Spider):
+    name = "Punch_scraper"
+    
+    
+    def start_requests(self):
+        urls = ["https://punchng.com/"]
+        
+        for url in urls:
+            yield Request(url, callback=self.link_follow)
+            
+            
+    def link_follow(self, response):
+        section = response.xpath("//*/section[@class='col-md-12 col-lg-6 latest-news-wraper']")
+        links = section.css("div.row > ul li a::attr(href)").extract() # xpath("./ul//li/a/@href").extract()
+        for link in links:
+            yield response.follow(url = link, callback=self.parse)
+        
+            
+    def parse(self, response):
+        title = response.css("h1.post_title::text").extract_first()
+        content = response.css("div.entry-content")
+        page_sum = content.xpath(".//p")
+        for pg in page_sum:
+            parag = " ".join(pg.xpath('.//text()').extract())
+            summary[title].append(parag)
+        key = list(summary.keys())
+        for i in key:
+            summary[i] = "\n".join(summary[i])
+        with open(f'{title}.txt', 'w') as doc:
+            doc.writelines(title+'\n')
+            doc.writelines(summary[title])
+            
+if __name__ == "__main__":
+    process = CrawlerProcess()
+    process.crawl(PunchScraper)
+    process.start()    
